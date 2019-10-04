e8f6a41288ebcaedf43c9b60466b42e32a224f19
Bob Kuhar <rkuhar@blueorigin.com>
2019-10-03 20:10:03 -0700

    CH02:  WIP to "Handling Invalid Input"

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,13 +1,33 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
-    // that mut makes guess mutable
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    // Oh, ooh.  Some sort of String interpolation here
-    println!("You guessed: {}", guess);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+        println!("Please input your guess.");
+        // that mut makes guess mutable
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        // Oh, ooh.  Some sort of String interpolation here
+        println!("Your guess: {}", guess);
+
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("Please type a number!");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
