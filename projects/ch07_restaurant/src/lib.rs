30c397ff49bf3724c86aab1e7d42a6baa788b44c
Bob Kuhar <rkuhar@blueorigin.com>
2020-01-07 22:14:35 -0800

    CH07: front_of_house modules to their own sourcefile 7-22 $ tree . ├── Cargo.lock ├── Cargo.toml └── src     ├── front_of_house.rs     └── lib.rs

--- projects/ch07_restaurant/src/lib.rs
+++ projects/ch07_restaurant/src/lib.rs
@@ -1,20 +1,9 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-
-        fn serve_order() {}
-
-        fn take_payment() {}
-    }
-}
 
 pub fn serve_order() {}
+
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
 
 mod back_of_house {
     fn fix_incorrect_order() {
